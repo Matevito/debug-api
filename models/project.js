e5606aeb1b997639857770bc3644e87199ae8db8
Mateo <madiazt@unal.edu.co>
2022-03-01 20:29:03 -0500

    skeleton of project model

--- models/project.js
+++ models/project.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const mongoose = require("mongoose");
 
 const projectSchema = mongoose.Schema({
-    title,
-    description,
-    team,
+    title: { type: String, required: true, minLength: 5, maxLength: 100},
+    description: { type: String, required: true, MaxLength: 500},
+    teamLeader: { type: mongoose.Schema.ObjectId, ref:"User", required: true},
+    team: [ { type: mongoose.Schema.ObjectId, ref:"User", required: true }],
 });
 
 const Project = mongoose.model("Project", projectSchema);
