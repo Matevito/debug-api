dcd272ed45bfbfd30e1c354b577864aab4e3d77e
Mateo <madiazt@unal.edu.co>
2022-03-01 20:32:14 -0500

    added skeleton of comment model

--- models/comment.js
+++ models/comment.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
-
-const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const commentSchema = mongoose.Schema({
-    issue,
-    user,
-    date,
-    message,
+    issue: { type: mongoose.Schema.ObjectId, ref:"Issue", required: true},
+    user: { type: mongoose.Schema.ObjectId, ref:"User", required: true},
+    date: { type: Date, default: DateTime.now() },
+    message: { type: String, required:true, minLength: 1, maxLength: 500 },
     // screenshot
 });
 
