c93cfcc6f49e11cd000427ba036a37acd65f4ae1
Mateo <madiazt@unal.edu.co>
2022-03-14 19:08:20 -0500

    solved bugg with team sanitazion data on middleware

--- dependencies/middlewares/sanitizeProject.js
+++ dependencies/middlewares/sanitizeProject.js
@@ -1,20 +1,32 @@
 const { body, validationResult } = require("express-validator");
 const User = require("../../models/user");
+var ObjectId = require('mongoose').Types.ObjectId;
 
 const sanitizeProject = [
     body("title", "A title for the project is required").trim().escape(),
     body("description", "A description is required").trim().escape(),
-    body("team.*").custom((team_list) => {
+    body("team.*").custom(async(team_member, { req }) => {
+        let team_list = req.body.team;
+
         // members do not repeat
         let counting_team = [];
         team_list.forEach(member => {
-            counting_team.push(member);
             if (counting_team.includes(member)){
-                throw new Error("some team members repeat");
+                throw new Error("Some team members repeat")
             }
-        })
-        console.log(team_list)
-        // the list is not clean
+            counting_team.push(member);
+        });
+
+        // team members are on db
+        if (!ObjectId.isValid.team_member) {
+            throw new Error("User id is corrupted")
+        };
+        let found_user = await User.findById(team_member);
+        if (!found_user) {
+            throw new Error("User is not on db")
+        }
+
+        // the id value is fine, continue
         return true;
     }).escape(),
     body("teamLeader").custom((teamLeader, { req }) => {
@@ -22,30 +34,19 @@ const sanitizeProject = [
         const team_list = req.body.team;
         if (!team_list.includes(teamLeader)) {
             // if is not on the list...
-            throw new Error("team leaderis not in the team list")
+            throw new Error("team leader is not in the team list")
         };
         // value is not null
         return true;
     }).escape(),
     (req, res, next) => {
         let errors = validationResult(req);
-        if (!errors.isEmpty) {
+        if (!errors.isEmpty()) {
             return res.status(400).json({
                 msg: "Error with parsed form data",
                 error: errors.array()
             })
         };
-        // team members are on db
-        const team_list = req.body.team;
-        console.log(req.body)
-        /*
-        team_list.forEach(async (member_id) => {
-            let found_user = await User.findOne({ _id: member_id});
-            if (!found_user) {
-                throw new Error("some team members do not exist")
-            }
-        });
-        */
         // parsed data is valid, continue
         next();
     }
