ee26f0641d9c3ba8f5da4408634760d4e8e8a48e
Mateo <madiazt@unal.edu.co>
2022-03-30 18:19:37 -0500

    setup issue_put test db

---
+++ __test__/issue_test/issue_put.test.js
@@ -0,0 +1,39 @@
+const api = require("../../routes/apiv1");
+const Issue = require("../../models/issue");
+const express = require("express");
+
+if (process.env.NODE_ENV !== 'production') require("dotenv").config();
+
+const request = require("supertest");
+const testDB = require("../mongoConfigTesting");
+// db set-up
+const setupDb = require("./setupDb");
+
+jest.setTimeout(30000);
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/", api);
+
+describe("PUT /issue/:id tests", () => {
+    let usersList;
+    let testProject;
+    let tokenList;
+    let issuesList;
+    
+    beforeAll(async() => {
+        await testDB();
+        const dbData = await setupDb.getData();
+        
+        testProject = dbData.project;
+        tokenList = dbData.tokenList;
+        usersList = dbData.usersList;
+        issuesList = dbData.issues;
+        issuesList.forEach(async(issue) => {
+            await issue.save()
+        })
+    })
+
+    test.todo("todo tests...")
+})
--- __test__/issue_test/setupDb.js
+++ __test__/issue_test/setupDb.js
@@ -1,4 +1,5 @@
 const Project = require("../../models/project");
+const Issue = require("../../models/issue")
 const get_token = require("../get_token");
 
 const userList = require("../userList").get_users;
@@ -37,16 +38,60 @@ const getTokens = (users) => {
     return tokenList
 
 }
+const getIssues = async(project) => {
+    const projectId = project._id
+    const issuesForm = [
+        {
+            title: "issue 1",
+            description: "an example of a description",
+            project: projectId,
+            status: "open",
+            priority: "mid",
+            type: "feature req",
+            screenshots: []
+        },
+        {
+            title: "issue 2",
+            description: "a description....",
+            project: projectId,
+            status: "open",
+            priority: "high",
+            type: "bugg-error",
+            screenshots: []
+        },
+        {
+            title: "issue 3",
+            description: "tests team functionalities...",
+            project: projectId,
+            status: "in progress",
+            priority: "high",
+            type: "feature req",
+            handlingTeam: [project.team[1]],
+            screenshots: []
+        }
+    ];
+    const issuesObj = []
+    issuesForm.forEach(async (form) => {
+        const new_issue = new Issue(form);
+        issuesObj.push(new_issue)
+        //await new_issue.save()
+    })
+
+    return issuesObj
+}
 exports.getData =  async () => {
     const user_list = await userList()
     const savedUsers = await saveUsers(user_list);
     const project = await getProject(savedUsers);
     const tokenList = getTokens(savedUsers);
-    //todo: create some test issues
+    
+    const issuesList = await getIssues(project)
+    
     return {
         usersList: savedUsers,
         project: project,
-        tokenList: tokenList
+        tokenList: tokenList,
+        issues: issuesList
     };
 
 }
