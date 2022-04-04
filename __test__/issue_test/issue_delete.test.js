484f1915d192631a31eb9ed0a1b00056742c6c60
Mateo <madiazt@unal.edu.co>
2022-04-03 19:33:19 -0500

    added tests of protection of DELETE issue controller

--- __test__/issue_test/issue_delete.test.js
+++ __test__/issue_test/issue_delete.test.js
@@ -48,12 +48,45 @@ describe("DELETE /issue/:id", () => {
             .set('Accept', 'application/json')
             .set({"auth-token": token});
 
+        expect(res.status).toEqual(401);
+        expect(res.body.error).toBe("Access denied")
+    });
+    test("handles developer inside the project", async() => {
+        const token = tokenList[2].token;
+        const issueId = issuesList[1]._id
+        const res = await request(app)
+            .delete(`/issue/${issueId}`)
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .set({"auth-token": token});
+
+        expect(res.status).toEqual(401);
+        expect(res.body.error).toBe("Access denied")
+    });
+    test("handles teamL of the project", async() => {
+        const token = tokenList[4].token;
+        const issueId = issuesList[1]._id
+        const res = await request(app)
+            .delete(`/issue/${issueId}`)
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .set({"auth-token": token});
+
+        expect(res.status).toEqual(401);
+        expect(res.body.error).toBe("Access denied")
+    });
+    // index = 0
+    test.only("handles a project that no longer exist on db", async() => {
+        const token = tokenList[0].token;
+        const issueId = issuesList[0]._id
+        const res = await request(app)
+            .delete(`/issue/${issueId}`)
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .set({"auth-token": token});
+
         console.log(res.body)
     });
-    test.todo("handles developer inside the project");
-    test.todo("handles teamL of the project");
-    // index = 0
-    test.todo("handles a project that no longer exist on db");
 
     test("admin can delete a proj and its related data", async() => {
         const token = tokenList[0].token;
