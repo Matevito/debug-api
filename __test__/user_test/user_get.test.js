7be9c1c60807435219ffb1f54d870db69d54f614
Mateo <madiazt@unal.edu.co>
2022-04-03 22:47:50 -0500

    added GET /user/:id tested controller

--- __test__/user_test/user_get.test.js
+++ __test__/user_test/user_get.test.js
@@ -1,3 +1,67 @@
+const api = require("../../routes/apiv1");
+const express = require("express");
+const User = require("../../models/user")
+if (process.env.NODE_ENV !== 'production') require("dotenv").config();
+
+const request = require("supertest");
+const testDB = require("../mongoConfigTesting");
+// db set-up
+const setupDb = require("../issue_test/setupDb");
+
+jest.setTimeout(30000);
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/", api);
+
 describe("GET /user/:id", () => {
-    test.todo("")
+    let usersList;
+    let testProject;
+    let tokenList;
+    let issuesList;
+
+    beforeAll(async() => {
+        await testDB();
+        const dbData = await setupDb.getData();
+        
+        testProject = dbData.project;
+        tokenList = dbData.tokenList;
+        usersList = dbData.usersList;
+        issuesList = dbData.issues;
+        issuesList.forEach(async(issue) => {
+            await issue.save()
+        })
+    });
+
+    test("handles user not on db", async() => {
+        const token = tokenList[0].token;
+        const userTest_id = usersList[3]._id;
+        await User.findByIdAndRemove(userTest_id);
+
+        const res = await request(app)
+            .get(`/user/${userTest_id}`)
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .set({"auth-token": token})
+
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe("User not found on db")
+    });
+    test("returns user on db", async() => {
+        const token = tokenList[0].token;
+        const userTest_id = usersList[4]._id;
+        const res = await request(app)
+            .get(`/user/${userTest_id}`)
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .set({"auth-token": token})
+
+        expect(res.status).toBe(200);
+        expect(res.body.error).toBe(null);
+        expect(res.body.msg).toBe("user info sent successfully")
+        expect(res.body.data.user._id == userTest_id).toBe(true)
+        expect(res.body.data.projects.list.length).toBe(1);
+        expect(res.body.data.issues.list.length).toBe(0)
+    });
 })
--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+const Issue = require("../models/issue");
+const Project = require("../models/project");
 const User = require("../models/user")
 
 exports.userList_get = async(req, res) => {
@@ -14,8 +16,49 @@ exports.userList_get = async(req, res) => {
         data: userList
     })
 };
-exports.user_get = (req, res) => {
+exports.user_get = async(req, res) => {
+    const userOnDB =  await User.findById(req.params.id);
+    if (!userOnDB) {
+        return res.status(400).json({
+            error: "User not found on db"
+        })
+    };
+    // user project info
+    const projectsOnDB = await Project.find({});
+    if (!projectsOnDB) {
+        return res.status(400).json({
+            error: "User data not found on db"
+        })
+    }
+    const userProjects = projectsOnDB.filter((proj) => {
+        return proj.team.includes(req.params.id);
+    });
+    // user issues info
+    const issuesOnDB = await Issue.find({});
+    if (!issuesOnDB) {
+        return res.status(400).json({
+            error: "User data not found on db"
+        })
+    }
+    const userIssues = issuesOnDB.filter((issue) => {
+        return issue.handlingTeam.includes(req.params.id)
+    })
 
+    res.json({
+        error: null,
+        msg: "user info sent successfully",
+        data: {
+            user: userOnDB,
+            projects: {
+                number: userProjects.length,
+                list: projectsOnDB,
+            },
+            issues: {
+                number: userIssues.length,
+                list: userIssues
+            }
+        }
+    });
 };
 exports.user_put = (req, res) => {
 
